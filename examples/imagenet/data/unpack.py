7de527471dca5078c564a06ace7da09ccd50fb09
Leonard Truong <leonardtruong@gmail.com>
2016-02-16 13:08:35 -0800

    Add val metadata

--- examples/imagenet/data/unpack.py
+++ examples/imagenet/data/unpack.py
@@ -6,7 +6,7 @@ def untar(file, output_dir):
     subprocess.check_call("tar -xf {file} -C {output_dir}".format(file=file, output_dir=output_dir).split())
 
 
-for dataset in ["ILSVRC2012_img_train.tar", "ILSVRC2012_img_test.tar"]:
+for dataset in ["ILSVRC2012_img_train.tar", "ILSVRC2012_img_val.tar"]:
     dataset_name, _ = dataset.split(".")
     os.mkdir("{dset}".format(dset=dataset_name))
 
